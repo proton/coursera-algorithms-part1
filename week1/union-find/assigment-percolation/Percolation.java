adc8d3ca2b74c3562459fbe46fb905490b09774a
Petr Savichev (proton) <psavichev@gmail.com>
2021-02-09 05:24:36 -0600

    mostly pass correctness test

--- week1/union-find/assigment-percolation/Percolation.java
+++ week1/union-find/assigment-percolation/Percolation.java
@@ -1,5 +1,3 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 // A full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) open sites.
@@ -18,38 +16,50 @@ public class Percolation {
         this.sites = new boolean[n][n];
         this.openCount = 0;
 
-        for(int i = 0; i < n; ++i)
-        for(int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j) {
             this.sites[i][j] = false;
         }
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        row -= 1;
+        col -= 1;
         if (!sites[row][col]) {
             sites[row][col] = true;
             this.openCount += 1;
 
             int ufPos = coordsToUf(row, col);
-            if (row > 0         ) uf.union(ufPos, coordsToUf(row - 1, col));
-            if (row < this.n - 1) uf.union(ufPos, coordsToUf(row + 1, col));
-            if (col > 0         ) uf.union(ufPos, coordsToUf(row, col - 1));
-            if (col < this.n - 1) uf.union(ufPos, coordsToUf(row, col + 1));
+            if (row > 0 && sites[row - 1][col]) uf.union(ufPos, coordsToUf(row - 1, col));
+            if (row < this.n - 1 && sites[row + 1][col]) uf.union(ufPos, coordsToUf(row + 1, col));
+            if (col > 0 && sites[row][col - 1]) uf.union(ufPos, coordsToUf(row, col - 1));
+            if (col < this.n - 1 && sites[row][col + 1]) uf.union(ufPos, coordsToUf(row, col + 1));
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
+        row -= 1;
+        col -= 1;
         return sites[row][col];
+    }
+
+    private void validateXY(int row, int col) {
+        if (row <= 0 || col <= 0 || row >= this.n || col >= this.n) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         if (!isOpen(row, col)) return false;
 
+        row -= 1;
+        col -= 1;
         int ufPos = coordsToUf(row, col);
         int rootA = uf.find(ufPos);
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             int rootB = uf.find(coordsToUf(0, i));
             if (rootA == rootB) return true;
         }
@@ -63,8 +73,8 @@ public class Percolation {
 
     // A full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) open sites.
     public boolean percolates() {
-        for(int i = 0; i < n; ++i) {
-            if (isFull(this.n - 1, i)) return true;
+        for (int i = 1; i <= n; ++i) {
+            if (isFull(this.n, i)) return true;
         }
         return false;
     }
