21473808923241e75e96fb4fc40b37f58bb059f4
Petr Savichev (proton) <psavichev@gmail.com>
2021-02-08 22:28:20 -0600

    clean warnings

--- week1/union-find/assigment-percolation/Percolation.java
+++ week1/union-find/assigment-percolation/Percolation.java
@@ -6,10 +6,10 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 // We say the system percolates if there is a full site in the bottom row. In other words, a system percolates if we fill all open sites connected to the top row and that process fills some open site on the bottom row. (For the insulating/metallic materials example, the open sites correspond to metallic materials, so that a system that percolates has a metallic path from top to bottom, with full sites conducting. For the porous substance example, the open sites correspond to empty space through which water might flow, so that a system that percolates lets water fill open sites, flowing from top to bottom.)
 
 public class Percolation {
-    int n;
-    WeightedQuickUnionUF uf;
-    boolean[][] sites;
-    int openCount;
+    private int n;
+    private WeightedQuickUnionUF uf;
+    private boolean[][] sites;
+    private int openCount;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
@@ -48,8 +48,10 @@ public class Percolation {
         if (!isOpen(row, col)) return false;
 
         int ufPos = coordsToUf(row, col);
+        int rootA = uf.find(ufPos);
         for(int i = 0; i < n; ++i) {
-            if (uf.connected(ufPos, coordsToUf(0, i))) return true;
+            int rootB = uf.find(coordsToUf(0, i));
+            if (rootA == rootB) return true;
         }
         return false;
     }
