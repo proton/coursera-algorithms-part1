de971d698fc3187ae7242dfe590a84ad75444b3b
Petr Savichev (proton) <psavichev@gmail.com>
2021-02-11 18:06:22 -0600

    task 1 (3 sums) solution

--- week1/analysis-of-algorithms/interview-questions/task1/TASK.md
+++ week1/analysis-of-algorithms/interview-questions/task1/TASK.md
@@ -1,3 +1,10 @@
 ### 3-SUM in quadratic time
 
 Design an algorithm for the 3-SUM problem that takes time proportional to n^2 in the worst case. You may assume that you can sort the n integers in time proportional to n^2n or better.
+
+Speed:
+
+```
+N     |  n^3  |  n^2  |
+1,000 |  9.07 | 0.035 |
+2,000 | 76.53 | 0.135 |
--- week1/analysis-of-algorithms/interview-questions/task1/solution.rb
+++ week1/analysis-of-algorithms/interview-questions/task1/solution.rb
@@ -3,26 +3,36 @@ require "table_print"
 
 def three_sum(numbers)
   cnt = 0
+  numbers.sort!
   (0...numbers.size).each do |i|
-    ((i + 1)...numbers.size).each do |j|
-      ((j + 1)...numbers.size).each do |k|
-        cnt += 1 if numbers[i] + numbers[j] + numbers[k] == 0
+    j = i + 1
+    k = numbers.size - 1
+    while j < k
+      case numbers[i] + numbers[j] + numbers[k] <=> 0
+      when 0
+        cnt += 1
+        j += 1
+        k -= 1
+      when 1
+        k -= 1
+      when -1
+        j += 1
       end
     end
   end
   cnt
 end
 
-files = Dir.glob("data/*")
+files = Dir.glob("data/*").sort
 results = {}
 
 files.each do |file|
-  puts file
+  # puts file
   numbers = File.open(file).each_line.map(&:to_i)
   result = nil
   time = Benchmark.measure { result = three_sum(numbers) }
   results[file] = [numbers.size, result, time.real.to_f]
-  p [file, results[file]]
+  # p [file, results[file]]
 end
 
 results.sort_by { |k, v| v[1] }.to_h.each do |file, (n, result, time)|
