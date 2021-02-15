4e9f78fbd3f2bc0c8021a648a678005e903e1d5b
Petr Savichev (proton) <psavichev@gmail.com>
2021-02-14 19:00:36 -0600

    some tests for RandomizedQueue

--- week2/stacks-and-queues/assigment-deques-and-randomized-queues/RandomizedQueue.java
+++ week2/stacks-and-queues/assigment-deques-and-randomized-queues/RandomizedQueue.java
@@ -18,12 +18,12 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     @Override
     public void remove() {
-        throw new UnsupportedOperationException();
+      throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean hasNext() {
-        return index < size - 1;
+      return index < size;
     }
 
     @Override
@@ -133,6 +133,31 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
   // unit testing (required)
   public static void main(String[] args) {
+    RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
+    assert queue.isEmpty() == true;
+    assert queue.size() == 0;
 
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+
+    assert queue.size() == 4;
+
+    Iterator<Integer> it = queue.iterator();
+    for (int i = 0; i < 4; ++i) {
+      assert it.hasNext() == true;
+      it.next();
+    }
+    assert it.hasNext() == false;
+
+    queue.dequeue();
+    assert queue.size() == 3;
+    queue.dequeue();
+    assert queue.size() == 2;
+    queue.dequeue();
+    assert queue.size() == 1;
+    queue.dequeue();
+    assert queue.size() == 0;
   }
 }
